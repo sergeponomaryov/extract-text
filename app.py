ae4dbf4790f9389d53de4e57dc17afec4e3a699e
sergeponomaryov <ponomaryovserge@gmail.com>
2020-11-06 16:10:46 +0000

    extension guessing and gitignore

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import urllib.request
+from os.path import splitext
+from urllib.parse import urlparse
 
 import textract
 from flask import Flask, jsonify, request
@@ -8,17 +10,24 @@ app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/extract', methods=['GET'])
 def extract():
+    supportedExts = ["csv", "doc", "docx", "eml", "epub", "gif", "jpg", "jpeg", "json", "html", "htm", "mp3", "msg", "odt", "ogg", "pdf", "png", "pptx", "ps", "rtf", "tiff", "tif", "txt", "wav", "xlsx", "xls"]
     url = None
 
     url = request.args.get('url', type=str)
-    ext = request.args.get('ext', type=str)
+    ext = request.args.get('extension', type=str)
 
     if url == None:
         return 'URL is required', 400
     if ext == None:
-        return 'Ext is required', 400
+        path = urlparse(url).path
+        ext = splitext(path)[1][1:]
+        if ext not in supportedExts:
+            return "Please pass the extension parameter", 400
 
-    # add guessing ext from url
+    if ext not in supportedExts:
+        return "Unsupported extension. Supported extensions: " + ', '.join(supportedExts), 400
+
+    # jsonify errors, and add try/catch for parsing
     # add binary file input
     # parse all weird characters etc, should be pure text? but also /n's and stuff.
 
