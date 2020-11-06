f6ca8da4d3b79ea485404aa78daf6d8595534824
sergeponomaryov <ponomaryovserge@gmail.com>
2020-11-06 16:51:04 +0000

    switch to json post

--- app.py
+++ app.py
@@ -8,28 +8,30 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
-@app.route('/extract', methods=['GET'])
+@app.route('/extract', methods=['POST'])
 def extract():
     supportedExts = ["csv", "doc", "docx", "eml", "epub", "gif", "jpg", "jpeg", "json", "html", "htm", "mp3", "msg", "odt", "ogg", "pdf", "png", "pptx", "ps", "rtf", "tiff", "tif", "txt", "wav", "xlsx", "xls"]
-    url = None
 
-    url = request.args.get('url', type=str)
-    ext = request.args.get('extension', type=str)
+    if not request.is_json:
+        return jsonify({"success": False, "error": "Missing JSON in request"}), 400
 
-    if url == None:
-        return 'URL is required', 400
-    if ext == None:
+    url = request.json.get("url", None)
+    ext = request.json.get("ext", None)
+
+    if not url:
+        return jsonify({"success": False, "error": "URL is required"}), 400
+    if not ext:
         path = urlparse(url).path
         ext = splitext(path)[1][1:]
         if ext not in supportedExts:
-            return "Please pass the extension parameter", 400
+            return jsonify({"success": False, "error": "Please pass the extension parameter"}), 400
 
     if ext not in supportedExts:
-        return "Unsupported extension. Supported extensions: " + ', '.join(supportedExts), 400
+        return jsonify({"success": False, "error": "Unsupported extension. Supported extensions: " + ', '.join(supportedExts)}), 400
 
-    # jsonify errors, and add try/catch for parsing
+    # jsonify errors, and add try/catch for loading and parsing
     # add binary file input
-    # parse all weird characters etc, should be pure text? but also /n's and stuff.
+    # mp3 parsing doesnt work - try deploying on lambda first..
 
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36')]
